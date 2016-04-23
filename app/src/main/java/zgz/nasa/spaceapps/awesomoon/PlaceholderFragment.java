7aab45bde4ffbefd58d47b99c95452b28f977926
novales35 <guillermocebollero@gmail.com>
2016-04-23 13:36:21 +0200

    Esqueleto basico

---
+++ app/src/main/java/zgz/nasa/spaceapps/awesomoon/PlaceholderFragment.java
@@ -0,0 +1,35 @@
+package zgz.nasa.spaceapps.awesomoon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Fragmento para el contenido principal
+ */
+public class PlaceholderFragment extends Fragment {
+    /**
+     * Este argumento del fragmento representa el título de cada
+     * sección
+     */
+    public static final String ARG_LAYOUT = "Layout";
+
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //TODO set layout
+        //View view = inflater.inflate(layout, container, false);
+
+        //return view;
+        return null;
+    }
+
+}
