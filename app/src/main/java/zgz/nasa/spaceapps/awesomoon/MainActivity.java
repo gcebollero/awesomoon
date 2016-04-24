c8c1dabe5c9f381cc5b460847ef46a2325b9c2f4
novales35 <guillermocebollero@gmail.com>
2016-04-24 11:57:08 +0200

    Set about as principal screen

--- app/src/main/java/zgz/nasa/spaceapps/awesomoon/MainActivity.java
+++ app/src/main/java/zgz/nasa/spaceapps/awesomoon/MainActivity.java
@@ -64,7 +64,9 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
+        Fragment f = new AboutFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, f).addToBackStack(null).commit();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
