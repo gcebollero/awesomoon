0f65a3bb7f7fb60cdc663b50ea6ed9346a871759
jgpjam <jamesarkon@gmail.com>
2016-04-24 11:56:04 +0200

    Changed CameraFragment to support v4

--- app/src/main/java/zgz/nasa/spaceapps/awesomoon/CameraFindExtractionFragment.java
+++ app/src/main/java/zgz/nasa/spaceapps/awesomoon/CameraFindExtractionFragment.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
